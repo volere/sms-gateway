c64cdb580d918757850681d4e9e8bead05f0c9e1
quercus <openai@cybersolutions.mozmail.com>
2023-06-18 15:40:43 -0400

    text message type

---
+++ types/sms.d.ts
@@ -0,0 +1,21 @@
+interface SMS {
+  ToCountry: string;
+  ToState: string;
+  SmsMessageSid: string;
+  NumMedia: string;
+  ToCity: string;
+  FromZip: string;
+  SmsSid: string;
+  FromState: string;
+  SmsStatus: string;
+  FromCity: string;
+  Body: string;
+  FromCountry: string;
+  To: string;
+  ToZip: string;
+  NumSegments: string;
+  MessageSid: string;
+  AccountSid: string;
+  From: string;
+  ApiVersion: string;
+}
