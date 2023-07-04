e017bcb561c7a5f2b339d65e16a3e384f0a77147
quercus <openai@cybersolutions.mozmail.com>
2023-07-04 10:20:20 -0400

    config

--- src/lib/util/index.ts
+++ src/lib/util/index.ts
@@ -1,5 +1,5 @@
 import { Stripe } from "stripe";
-
+import { Configuration, OpenAIApi } from "openai";
 export const ROOT_URL =
   process.env.VERCEL_ENV == "development"
     ? `${process.env.VERCEL_URL}`
@@ -11,3 +11,31 @@ export const stripeConfig = new Stripe(process.env.STRIPE as string, {
   //@ts-ignore "2022-08-01"
   apiVersion: null,
 });
+
+export async function ChatGPTRes(body: string) {
+  console.log("content", body);
+  try {
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI as string,
+    });
+    const openai = new OpenAIApi(configuration);
+    const result = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "user",
+          content: body,
+        },
+      ],
+    });
+    console.log(result.data.choices[0].message?.content);
+    if (typeof result.data.choices[0].message?.content != "string") {
+      throw new Error("GPT ERROR< SERVER DOWN");
+    }
+
+    return result.data.choices[0].message?.content;
+  } catch (err: any) {
+    console.error(err.response);
+    return "Error occurred while communicating with OpenAI GPT-4";
+  }
+}
